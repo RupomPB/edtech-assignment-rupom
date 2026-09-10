0188e2d8a85bf58cbf7de0d5fc283402cecd63c5
Rupom PB <rupombadhan111@gmail.com>
2026-09-10 11:34:29 +0600

    added banner section

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -20,7 +20,7 @@ export default function RootLayout({ children }) {
           <Navbar />
         </header>
 
-        <main className="py-2 md:w-11/12 mx-auto">{children}</main>
+        <main className="py-2 md:w-11/12 mx-auto min-h-[calc(100vh-302px)]">{children}</main>
 
         <footer>
           <Footer />
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,10 +1,11 @@
+import Banner from "@/components/home/Banner";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex flex-col flex-1 items-center justify-center ">
-      <buttton className=" btn btn-primary font-bold ">test button</buttton>
+    <div className="">
       
+      <section><Banner></Banner></section>
     </div>
   );
 }
