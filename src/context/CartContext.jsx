6f83241bc73c3f283f0a8b8fd04e0a84de64f1cc
Rupom PB <rupombadhan111@gmail.com>
2026-09-13 02:30:41 +0600

    save to localstorage, add item to cart

--- src/context/CartContext.jsx
+++ src/context/CartContext.jsx
@@ -1,11 +1,65 @@
 "use client"
 
-import React, { createContext, useContext, useState } from 'react';
+import { useSession } from 'next-auth/react';
+import React, { createContext, useContext, useEffect, useState } from 'react';
 
 const CartContext = createContext();
 
 export const CartProvider = ({children}) =>{
+    const {data: session} = useSession();
+
     const [cartItems, setCartItems] = useState([]);
+
+    // islogin user cart load 
+    useEffect(()=>{
+
+       if (!session?.user?.id) {
+      setCartItems([])
+      return;
+    }
+
+    const cartKey= `edtech-cart-${session.user.id}`;
+
+    // localStorage
+    const savedCart = localStorage.getItem(cartKey);
+
+    if(savedCart){
+        setCartItems(JSON.parse(savedCart));
+    }else{
+        setCartItems([]);
+    }
+    },[session]);
+
+    // if cart change localstorage will save
+    useEffect(() => {
+    
+        if(!session?.user?.id) return;
+
+        const cartKey =`edtech-cart-${session.user.id}`
+
+    // set to localstorage
+    localStorage.setItem(cartKey, JSON.stringify(cartItems));
+
+    }, [cartItems, session]);
+
+    // add item to cart
+    const addToCart = (item)=>{
+        setCartItems((previousItems)=>{
+            const alreadyExists = previousItems.some(
+                (cartItem)=>
+                    cartItem.id === item.id && cartItem.type === item.type
+            );
+
+            if(alreadyExists){
+                return previousItems
+            }
+            return [...previousItems, item];
+
+        });
+    };
+
+    
+    
 
     return (
         <CartContext.Provider
